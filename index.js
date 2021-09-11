2d666b24d425e082e76f2798e04fd2f9391ee79c
w3sl1y <SV73623892@idat.edu.pe>
2021-09-11 12:11:12 -0500

    Actualizado

--- index.js
+++ index.js
@@ -17,3 +17,71 @@ let keys = Array.from(map.keys());
 keys.push("more");
 
 alert(keys);
+
+//Ejercicio #3
+let i = 3;
+while (i) { alert( i-- );}
+//El últimmo valor alertado será el número 0, porque while(i) detendrá el bucle cuando i llegue a su valor 0
+
+//Ejercicio #4
+let i = 0;while (++i < 5) alert( i ); //Muestra valores desde el 1 al 4 y al llegar a 5 se detiene
+
+let i = 0;while (i++ < 5) alert( i ); //Muestra valores desde el 1 al 5 y se detiene pero este último es falso
+
+//Ejercicio #5
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+  }
+  
+  ask(
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+  );
+
+  //Ejercicio #6
+  function makeCounter() {
+    let count = 0;
+  
+    function counter() {
+      return count++;
+    }
+  
+    counter.set = value => count = value;
+  
+    counter.decrease = () => count--;
+  
+    return counter;
+  }
+
+  //Ejercicio #7
+  let num;
+
+do {
+  num = prompt("Ingrese un número mayor que 100?", 0);
+} while (num <= 100 && num);
+
+//Ejercicio #8
+//Bucle
+function printList(list) {
+    let tmp = list;
+  
+    while (tmp) {
+      alert(tmp.value);
+      tmp = tmp.next;
+    }
+  }
+  printList(list);
+
+//Recursividad
+function printList(list) {
+
+    alert(list.value);
+  
+    if (list.next) {
+      printList(list.next);
+    }
+  }
+  printList(list);
+  //Cualquiera de las dos formas es buena, pero el bucle es más efectivo
