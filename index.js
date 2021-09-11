157a7f90329d99661718c77ac7e340ba4c8eee40
w3sl1y <SV73623892@idat.edu.pe>
2021-09-11 10:28:18 -0500

    Actualización

---
+++ index.js
@@ -0,0 +1,19 @@
+//Ejercicio #0
+function unique(arr) {
+    return Array.from(new Set(arr));
+  }
+
+//Ejercicio #1
+
+
+
+//Ejercicio #2
+let map = new Map();
+
+map.set("name", "John");
+
+let keys = Array.from(map.keys());
+
+keys.push("more");
+
+alert(keys);
